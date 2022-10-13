6e842e8cba80656483c9c5f2bea4ced2614e78cd
Pedro Botter <pedrobotter2016@gmail.com>
2022-10-13 09:40:57 -0300

    primeira versao

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-2xl ">Hello Worl</h1>
+      <button className=""> </button>
+    </>
+  )
+}
